ddced781867e17cc1ee49184f23d9d3d977f80b9
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-03-26 20:13:15 -0400

    recieving errors on previously workig code

--- debug.py
+++ debug.py
@@ -6,12 +6,18 @@ from lib import *
 
 r1 = Role("Programmer Kid")
 r2 = Role("Bride")
+r3 = Role("Joker")
 
 
 a1 = Audition("Morgan", "NYC", True, r1)
 a2 = Audition("Mike", "CO", False, r1)
-a3 = Audition("Chandler", "NC", False, r2)
-a2 = Audition("Raiza", "MT", True, r2)
+
+a3 = Audition("Chandler", "NC", True, r2)
+a4 = Audition("Raiza", "MT", True, r2)
+
+a5 = Audition("Dan", "NC", False, r3)
+a5 = Audition("Dan", "NC", False, r3)
+
 
 
 # the below line allows us to stop & try stuff!
--- lib/role.py
+++ lib/role.py
@@ -3,27 +3,27 @@ from .audition import Audition
 class Role:
 
 
-    def __init__(self, character_name:str):
+    def __init__(self, character_name):
         self.character_name = character_name
     
 #  returns all of the auditions associated with this role.
     @property
     def auditions(self):
-        return [a for a in Audition.all if a.role == self]
+        return [a.actor for a in Audition.all if a.role == self]
     
 # #     returns a list of names from the actors associated with this role.
     @property
     def actors(self):
-        return [a.actor for a in self.auditions if a.role == self]
+        return [a.actor for a in self.auditions]
     
 #     # returns a list of locations from the auditions associated with this role.
     @property
     def locations(self):
-        return [l.location for l in self.auditions if l.role == self]
+        return [l.location for l in self.auditions]
     
 #     # returns the first instance of the audition that was hired for this role or 
 #     # returns a string 'no actor has been hired for this role'.
-    
+    @property
     def lead(self):
         role_lead = "no actor has been hired for this role"
         for role in self.auditions:
@@ -32,3 +32,30 @@ class Role:
                return role_lead
         else:
             return role_lead
+        
+    # @property
+    # def hired_actor(self):
+    #     return[a.actor for a in self.auditions if a.hired == True]
+    
+    # @property
+    # def leading_actor(self):
+    #     if len(self.hired_actor) > 0:
+    #         return "no actor has been hired for this role"
+    #     else:
+    #         return self.hired_actor[0]
+#   returns the second instance of the audition that was hired for this role or 
+#   returns a string 'no actor has been hired for understudy for this role'.
+
+    # @property
+    # def understudy(self):
+    #     support = [a for a in self.auditions if a.hired]
+    #     wish_me_luck = "no actor has been hired for understudy for this role"
+    #     if len(support) > 1:
+    #         return support[1]
+    #     else:
+    #         return wish_me_luck
+
+        # if len(self.the_spare) >= 1:
+        #     return self.the_spare[1]
+        # else:
+        #     return wish_me_luck
